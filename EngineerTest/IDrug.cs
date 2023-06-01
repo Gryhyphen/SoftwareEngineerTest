d0d2deffe26472142acbb923bf007550d0746789
Gry- <gryhyphen@ninoa.lan>
2023-06-01 16:41:10 +1000

    Lint drugs

--- EngineerTest/IDrug.cs
+++ EngineerTest/IDrug.cs
@@ -1,8 +1,26 @@
+// <copyright file="IDrug.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
 namespace EngineerTest;
 
+/// <summary>
+/// An interface for an instance of a drug.
+/// </summary>
 public interface IDrug
 {
+    /// <summary>
+    /// Gets name.
+    /// </summary>
     string Name { get; }
+
+    /// <summary>
+    /// Gets or sets ExpiresIn which denotes the number of days we have until the item expires.
+    /// </summary>
     int ExpiresIn { get; set; }
+
+    /// <summary>
+    /// Gets or sets benefit which denotes how powerful the drug is.
+    /// </summary>
     int Benefit { get; set; }
 }
